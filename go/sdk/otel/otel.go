4f0dfa85769d3eab944917c71b423cbb76c49a5b
Anthony Salemo <ajssalemo@gmail.com>
2024-11-22 16:28:08 -0500

    adding a containers go/fiber + otel sdk application

--- go/sdk/otel/otel.go
+++ go/sdk/otel/otel.go
@@ -3,9 +3,14 @@ package otel
 import (
 	"context"
 	"errors"
+	"os"
 	"time"
+	"fmt"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
@@ -43,7 +48,7 @@ func SetupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, er
 	otel.SetTextMapPropagator(prop)
 
 	// Set up trace provider.
-	tracerProvider, err := newTraceProvider()
+	tracerProvider, err := newTraceProvider(context.Background())
 	if err != nil {
 		handleErr(err)
 		return
@@ -52,7 +57,7 @@ func SetupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, er
 	otel.SetTracerProvider(tracerProvider)
 
 	// Set up meter provider.
-	meterProvider, err := newMeterProvider()
+	meterProvider, err := newMeterProvider(context.Background())
 	if err != nil {
 		handleErr(err)
 		return
@@ -61,7 +66,7 @@ func SetupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, er
 	otel.SetMeterProvider(meterProvider)
 
 	// Set up logger provider.
-	loggerProvider, err := newLoggerProvider()
+	loggerProvider, err := newLoggerProvider(context.Background())
 	if err != nil {
 		handleErr(err)
 		return
@@ -78,10 +83,29 @@ func newPropagator() propagation.TextMapPropagator {
 		propagation.Baggage{},
 	)
 }
+// Trace provider
+func newTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
+	// Use stdout exporter in development.
+	if os.Getenv("ENVIRONMENT") == "dev" {
+		traceExporter, err := stdouttrace.New(
+			stdouttrace.WithPrettyPrint())
 
-func newTraceProvider() (*trace.TracerProvider, error) {
-	traceExporter, err := stdouttrace.New(
-		stdouttrace.WithPrettyPrint())
+		if err != nil {
+			return nil, err
+		}
+
+		traceProvider := trace.NewTracerProvider(
+			trace.WithBatcher(traceExporter,
+				// Default is 5s. Set to 1s for demonstrative purposes.
+				trace.WithBatchTimeout(time.Second)),
+		)
+
+		fmt.Println("Using console trace exporter")
+		return traceProvider, nil
+	}
+	// Otherwise use otlp exporter
+	traceExporter, err := otlptracegrpc.New(ctx)
+
 	if err != nil {
 		return nil, err
 	}
@@ -91,11 +115,30 @@ func newTraceProvider() (*trace.TracerProvider, error) {
 			// Default is 5s. Set to 1s for demonstrative purposes.
 			trace.WithBatchTimeout(time.Second)),
 	)
+
+	fmt.Println("Using otlp trace exporter")
 	return traceProvider, nil
 }
+// Metric provider
+func newMeterProvider(ctx context.Context) (*metric.MeterProvider, error) {
+	// Use stdout exporter in development
+	if os.Getenv("ENVIRONMENT") == "dev" {
+		metricExporter, err := stdoutmetric.New()
+		if err != nil {
+			return nil, err
+		}
 
-func newMeterProvider() (*metric.MeterProvider, error) {
-	metricExporter, err := stdoutmetric.New()
+		meterProvider := metric.NewMeterProvider(
+			metric.WithReader(metric.NewPeriodicReader(metricExporter,
+				// Default is 1m. Set to 3s for demonstrative purposes.
+				metric.WithInterval(3*time.Second))),
+		)
+
+		fmt.Println("Using console metric exporter")
+		return meterProvider, nil
+	}
+	// Otherwise use otlp exporter
+	metricExporter, err := otlpmetricgrpc.New(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -105,11 +148,29 @@ func newMeterProvider() (*metric.MeterProvider, error) {
 			// Default is 1m. Set to 3s for demonstrative purposes.
 			metric.WithInterval(3*time.Second))),
 	)
+
+	fmt.Println("Using otlp metric exporter")
 	return meterProvider, nil
 }
+// Logger provider
+func newLoggerProvider(ctx context.Context) (*log.LoggerProvider, error) {
+	// Use stdout exporter in development
+	if os.Getenv("ENVIRONMENT") == "dev" {
+		logExporter, err := stdoutlog.New()
+		if err != nil {
+			return nil, err
+		}
 
-func newLoggerProvider() (*log.LoggerProvider, error) {
-	logExporter, err := stdoutlog.New()
+		loggerProvider := log.NewLoggerProvider(
+			log.WithProcessor(log.NewBatchProcessor(logExporter)),
+		)
+
+		fmt.Println("Using console log exporter")
+		return loggerProvider, nil
+	}
+
+	// Otherwise use otlp exporter
+	logExporter, err := otlploggrpc.New(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -117,5 +178,8 @@ func newLoggerProvider() (*log.LoggerProvider, error) {
 	loggerProvider := log.NewLoggerProvider(
 		log.WithProcessor(log.NewBatchProcessor(logExporter)),
 	)
+
+	fmt.Println("Using otlp log exporter")
 	return loggerProvider, nil
+
 }
